2d9236e395266914761f0e50014113968ba96fb9
Koti Vellanki <vellankikoti@gmail.com>
2024-11-07 17:28:45 +0530

    Update recordModel.js

--- backend/src/models/recordModel.js
+++ backend/src/models/recordModel.js
@@ -1,1 +1,18 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE_URL, { dialect: 'postgres' });
+
+const Record = sequelize.define('Record', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+}, {
+  timestamps: true,
+});
+
+module.exports = Record;
 

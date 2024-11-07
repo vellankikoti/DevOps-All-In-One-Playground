b68eeded8bcb44601cd108b0642f189bcea908b7
Koti Vellanki <vellankikoti@gmail.com>
2024-11-07 18:20:29 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,20 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_eks_cluster" "devops_cluster" {
+  name     = "devops-cluster"
+  role_arn = var.eks_role_arn
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+}
+
+resource "aws_eks_node_group" "devops_node_group" {
+  cluster_name    = aws_eks_cluster.devops_cluster.name
+  node_role_arn   = var.eks_node_role_arn
+  subnet_ids      = var.subnet_ids
+  instance_type   = "t3.medium"
+  desired_capacity = 2
+}
 

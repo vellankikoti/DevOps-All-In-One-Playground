74b3c092ea6a6092ed0fd1c2abc8ca7a7992d120
Koti Vellanki <vellankikoti@gmail.com>
2024-11-07 17:16:37 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import DataList from './components/DataList';
+import CreateForm from './components/CreateForm';
 
+function App() {
+  return (
+    <div className="App">
+      <h1>DevOps All-In-One Playground</h1>
+      <CreateForm />
+      <DataList />
+    </div>
+  );
+}
+
+export default App;
